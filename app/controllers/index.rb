d603415d58363b69f59656d960582ce2e2d2a403
bbeaird <bbeaird@gmail.com>
2013-11-04 16:17:51 -0800

    .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,27 +1,40 @@
 get '/' do
-  # Look in app/views/index.erb
   @notes = Note.all
   erb :index
 end
 
 post '/' do
+  # @notes = Note.all
   Note.create!(params)
   erb :index
 end
 
-get '/:id/edit' do
-  # p "*"*150
+get '/:id' do
   @note = Note.find(params[:id])
-  # p @note
+  erb :note_contents
+end
+
+get '/:id/edit' do
+  @note = Note.find(params[:id])
   @title = "Edit note ##{params[:id]}"
   erb :edit_note
 end
 
 put '/:id' do
-  p '*'*80
-  p params
   @note = Note.find(params[:id])
   @note.content = params[:content]
   @note.save
+  redirect '/'
+end
+
+get '/:id/delete' do
+  @note = Note.find(params[:id])
+  @title = "Confirm delete of note ##{params[:id]}"
+  erb :delete
+end
+
+delete '/:id/delete' do
+  @note = Note.find(params[:id])
+  @note.destroy
   redirect '/'
 end
